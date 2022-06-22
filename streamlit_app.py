b3fcf2a8629a4ccf59901f85a0b654523f786e23
josepedrosdg <108010044+josepedrosdg@users.noreply.github.com>
2022-06-22 15:51:34 +0100

    add multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,8 @@ sl.header('🥭 Another Header 🥝')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 sl.dataframe(my_fruit_list)
